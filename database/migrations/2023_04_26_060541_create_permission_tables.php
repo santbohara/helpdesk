a01e4826599f8c34967b2d26085735c22a2810ce
santbohara <santbohara@gmail.com>
2024-12-28 12:14:37 +0545

    Package Updates and Other Bug Fixes

--- database/migrations/2023_04_26_060541_create_permission_tables.php
+++ database/migrations/2023_04_26_060541_create_permission_tables.php
@@ -27,7 +27,7 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             // $table->bigIncrements('id'); // permission id
-            $table->uuid('id')->primary('id'); // permission id
+            $table->uuid('id')->primary(); // permission id
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
